63ca397b122a7c6d9520daf475a11b44e0579b3a
Carlos Interone <carlos@mostlyclicks.com>
2014-03-04 11:56:09 -0600

    add sendgrid configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,15 +19,15 @@ Ir02::Application.configure do
   end
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'mostlyclicks.com',
-    user_name:            ENV['GMAIL_USER'],
-    password:             ENV['GMAIL_PASS'],
-    authentication:       'plain',
-    enable_starttls_auto: true  
-  }
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,19 +50,19 @@ Ir02::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   if config.respond_to?(:action_mailer)
-    # config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.raise_delivery_errors = true
   end
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'mostlyclicks.com',
-    user_name:            ENV['GMAIL_USER'],
-    password:             ENV['GMAIL_PASS'],
-    authentication:       'plain',
-    enable_starttls_auto: true  
-  }
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 
   # Enable threaded mode
   # config.threadsafe!
