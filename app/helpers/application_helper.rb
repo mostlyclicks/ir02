efeedda79ff285308c9a10e329c74ac7bbd5ceac
Carlos Interone <carlos@mostlyclicks.com>
2014-03-04 20:08:37 -0600

    add submenu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
     presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
     presenter.menu_tag = "div class=\"collapse navbar-collapse\" id=\"main-nav-collapse\""
     presenter.list_tag = "ul class=\"nav navbar-nav pull-right pull-left-xs\""
-    presenter.max_depth = 0
+    presenter.max_depth = 2
     presenter
   end
 
@@ -22,4 +22,48 @@ module ApplicationHelper
     presenter.max_depth = 0
     presenter
   end
+
+  # def sub_menu_pages(page)
+  #   Refinery::Menu.new(refinery_menu_pages.detect{ |item| item.original_id == page.id }.children)
+  # end
+
+  # def sub_menu_pages(page)
+  #   presenter = Refinery::Menu.new(refinery_menu_pages.detect{ |item| item.original_id == page.id}.children)
+  #   #presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages.detect{ |item| item.original_id == page.id}.children)
+  #   presenter.menu_tag = "div class=\"collapse navbar-collapse\" id=\"main-nav-collapse\""
+  #   presenter.list_tag = "ul class=\"nav nav-pills\""
+  #   presenter.max_depth = 1
+  #   presenter
+  # end
+
+  def sub_menu_pages
+    sub_menu_items = Refinery::Menu.new(refinery_menu_pages.detect{ |item| item.original_id == @page.id }.children)
+    #sub_menu_items = Refinery::Menu.new(refinery_menu_pages.select{ |item| item.parent_id == @page.root.id })
+    # style = page_style(@page)
+    presenter = Refinery::Pages::MenuPresenter.new(sub_menu_items, self)
+    presenter.menu_tag = :div
+    # presenter.css = "#{style}"
+    presenter.list_tag = "ul class=\"nav nav-pills nav-stacked \""
+    # presenter.list_tag_css = "nav nav-pills nav-stacked"
+    presenter.max_depth = 6
+    first_css = :first
+    last_css = :last
+    presenter
+  end
+
+    def child_sub_menu_pages
+    #This works on parent and sub pages -- use 'select' instead of 'detect'
+    sub_menu_items = Refinery::Menu.new(refinery_menu_pages.select{ |item| item.parent_id == @page.root.id })
+    # style = page_style(@page)
+    presenter = Refinery::Pages::MenuPresenter.new(sub_menu_items, self)
+    presenter.menu_tag = :div
+    # presenter.css = "#{style}"
+    presenter.list_tag = "ul class=\"nav nav-pills nav-stacked \""
+    # presenter.list_tag_css = "nav nav-pills nav-stacked"
+    presenter.max_depth = 6
+    first_css = :first
+    last_css = :last
+    presenter
+  end
+
 end
