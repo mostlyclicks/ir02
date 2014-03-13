9f60fc0ece5b5536ac38a0449c5504c91a60a0ec
Carlos Interone <carlos@mostlyclicks.com>
2014-03-12 21:49:14 -0500

    products section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :get_random_testimonial
+
+
+  
+
+  protected
+
+    def get_random_testimonial
+      testimonials = Refinery::Testimonials::Testimonial.all
+      @random_testimonial = testimonials.sample
+    end
 end
